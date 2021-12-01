2533d9ecfd07876efd8d3962806ac509b0043e87
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-01 16:14:03 -0700

    added picture html element to hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,41 +2,59 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const Hero = () => {
-  return (
-    <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<picture>
+				<source
+					type="image/avif"
+					srcSet={`
+            /images/hero-img.avif  1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x
+          `}
+				/>
+				<source
+					type="image/jpeg"
+					srcSet={`
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x
+          `}
+				/>
+				<HeroImage src="/images/hero-img.jpg" />
+			</picture>
+			<Swoop src="/swoop.svg" />
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.section`
-  position: relative;
-  height: 60vh;
-  min-height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  background: hsl(0deg 0% 1%);
+	position: relative;
+	height: 60vh;
+	min-height: 400px;
+	display: flex;
+	justify-content: center;
+	align-items: flex-end;
+	background: hsl(0deg 0% 1%);
 `;
 
 const HeroImage = styled.img`
-  display: block;
-  width: 500px;
-  height: 500px;
-  max-height: 100%;
+	display: block;
+	width: 500px;
+	height: 500px;
+	max-height: 100%;
 `;
 
 const Swoop = styled.img`
-  position: absolute;
-  left: 0;
-  right: 0;
-  /*
+	position: absolute;
+	left: 0;
+	right: 0;
+	/*
     Overhang by a couple px to prevent any pixel-rounding
     display issues
   */
-  bottom: -2px;
-  width: 100%;
+	bottom: -2px;
+	width: 100%;
 `;
 
 export default Hero;
